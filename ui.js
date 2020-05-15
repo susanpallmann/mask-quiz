512d224dcd431b57c885a4a5271c3a19f31606b6
Susan Pallmann <38414619+susanpallmann@users.noreply.github.com>
2020-05-14 23:15:46 -0400

    Update ui.js

--- ui.js
+++ ui.js
@@ -4,13 +4,12 @@ $(document).ready(function() {
     $(".check").click(function() {
         // If the box is checked
         if ($(this).attr("aria-checked") === "true" ) {
-            // Uncheck it
-            $(this).attr("aria-checked", "false");
             // Calls function to update Bigfoot's happy meter (decrease it)
             //updateBigfoot(-1);
         // If the box isn't checked
         } else {
             // Check it
+            $(this).parent().find('.check').attr("aria-checked", "false");
             $(this).attr("aria-checked", "true");
             // Calls function to update Bigfoot's happy meter (increase it)
             //updateBigfoot(1);
