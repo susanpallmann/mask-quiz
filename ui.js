f114d2a22c5d742a5901ff5d098180916cc19d0d
Susan Pallmann <38414619+susanpallmann@users.noreply.github.com>
2020-05-14 23:25:12 -0400

    Update ui.js

--- ui.js
+++ ui.js
@@ -4,15 +4,15 @@ $(document).ready(function() {
     $(".check").click(function() {
         // If the box is checked
         if ($(this).attr("aria-checked") === "true" ) {
-            // Calls function to update Bigfoot's happy meter (decrease it)
-            //updateBigfoot(-1);
+            // Calls function to activate the next button
+            nextQ();
         // If the box isn't checked
         } else {
             // Check it
             $(this).parent().find('.check').attr("aria-checked", "false");
             $(this).attr("aria-checked", "true");
-            // Calls function to update Bigfoot's happy meter (increase it)
-            //updateBigfoot(1);
+            // Calls function to activate the next button
+            nextQ();
         }
     });
     // For accessibility, tracks the use of return/space key to replace a mouse click event, listens for key press
@@ -24,4 +24,16 @@ $(document).ready(function() {
             $(this).click();
         }
     });
+    $('#next').click(function() {
+        if ( $(this).attr('ready') === 'true' ) {
+            var currentQ = parseInt($(this).attr("q"));
+            var nextQ = currentQ + 1;
+            $('#q' + currentQ).slideToggle();
+            $('#q' + nextQ).slideToggle();
+        } else {
+        }
+    });
 });
+function nextQ() {
+    $('#next').attr('ready','true');
+}
